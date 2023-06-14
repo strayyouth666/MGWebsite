5d953ac1afce6d33eb56982de36e2f2b9a995e96
76687383+strayyouth666@users.noreply.github.com <76687383+strayyouth666@users.noreply.github.com>
2023-06-14 16:05:40 +0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li> */}
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/mulyosarigroup/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
